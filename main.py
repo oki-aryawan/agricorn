2ddae13bf80eeebf36a4dd5b4c5048d44c22b3ac
oki-aryawan <sandikalakual@gmail.com>
2022-11-09 22:44:25 +0700

    add result.save() custom

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import torch
+import cv2
+from PIL import Image
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/agricorn.pt')
 
 img = 'data/zsehat6.jpg'
 
-result = model(img)
+result = model(img, size=640)
+result.save(save_dir='result/')
 
+print(result.xyxy[0])
 print(result.pandas().xyxy[0])
