66b17c23c27971b7f342cfa1e45bfb1329ce5986
oki-aryawan <sandikalakual@gmail.com>
2023-05-28 09:50:23 +0700

    coba bang yuvicho

--- load-torch.py
+++ load-torch.py
@@ -1,11 +1,9 @@
 import torch
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/best-5.pt')
-x = 0
-
-while x <= 9:
-    x += 1
-    file = f'data/data-for-test/h{x}.jpg'
-    result = model(file)
-    result.save(save_dir='data/test-result')
-print('Done')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/yuvicho.pt')
+file = f'data/data-for-test/bawang.jpg'
+result = model(file)
+result.print()
+result.save()
+print(result.xyxy[0])  # im1 predictions (tensor)
+print(result.pandas().xyxy[0])
